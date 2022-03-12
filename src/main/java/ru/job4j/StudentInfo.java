d203e05d1bcad6ee33628a539a3d54b9fc697495
kamin <kaminoonte00@gmail.com>
2022-03-12 22:54:52 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Lavrentev Ivan");
+        System.out.println("01.06.2000");
+    }
+}
